3361e9df23ac9bc1403020a9de990a2eb75b069c
zakkiya <fnszakkiya@gmail.com>
2022-04-24 10:32:42 +0700

    menambahkan file formtambah.php

---
+++ formtambah.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>FROM TAMBAH</title>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+
+    <h1>FROM TAMBAH</h1>
+        <br><br><br><br><br><br>
+                <form action="action.php" method="post" class=" border p-3 mt-3">
+                        <label>NAMA : </label>
+                        <input type="text" name="nama_097" class="form-control"><br>
+                        <label>EMAIL : </label>
+                        <input type="text" name="email_097" class="form-control"><br>
+                        <label>ALAMAT : </label>
+                        <input type="text" name="alamat_097" class="form-control"><br>
+                        <button type="submit" class="btn btn-info"> TAMBAH DATA MAHASISWA</button>
+                </form>
+	</body>
+</html>
